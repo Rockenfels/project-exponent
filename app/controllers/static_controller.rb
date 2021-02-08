9a70fd458a7f5c025f4eb6cbac5fc1fe5c5abf34
Ray O <52436369+Rockenfels@users.noreply.github.com>
2021-02-08 01:23:14 -0700

    created base view files; added static actions

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,13 @@
 class StaticController < ApplicationController
+    def index
+        render :index
+    end
+
+    def about
+        render :about
+    end
+
+    def contact
+        render :contact
+    end
 end
